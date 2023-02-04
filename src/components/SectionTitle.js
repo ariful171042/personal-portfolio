6427b79c7f7e16fc495ee17a3bb5688bed9daaa9
ariful171042 <arif171042@gmail.com>
2023-02-04 13:02:52 +0600

    Skill section Done

---
+++ src/components/SectionTitle.js
@@ -0,0 +1,11 @@
+const SectionTitle = ({ title }) => {
+  return (
+    <div>
+      <div className="overflow-hidden">
+        <h2 className="section-title">{title}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
